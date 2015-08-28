e7047119c6878613eb6b61726e404c0d28d609bd
Andrew <asolesa@yahoo.ca>
2015-08-27 23:21:36 -0700

    Update Program.cs

--- RDB/Program.cs
+++ RDB/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using System.Reflection;
+using System.IO;
 
 namespace RDB
 {
@@ -16,17 +17,65 @@ namespace RDB
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
             Application.Run(new Form1());
         }
 
         static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
-            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("RDB.HtmlAgilityPack.dll"))
+            try
             {
-                byte[] data = new byte[stream.Length];
-                stream.Read(data, 0, data.Length);
-                return Assembly.Load(data);
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("RDB.Properties.Resources.HtmlAgilityPack.dll"))
+                {
+                    byte[] data = new byte[stream.Length];
+                    stream.Read(data, 0, data.Length);
+                    return Assembly.Load(data);
+                }
             }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        static Assembly OnResolveAssembly(object sender, ResolveEventArgs e)
+        {
+            var thisAssembly = Assembly.GetExecutingAssembly();
+
+            var assemblyName = new AssemblyName(e.Name);
+            var dllName = assemblyName.Name + ".dll";
+
+            var resources = thisAssembly.GetManifestResourceNames().Where(s => s.EndsWith(dllName));
+
+            if (resources.Any())
+            {
+                var resourceName = resources.First();
+
+                using (var stream = thisAssembly.GetManifestResourceStream(resourceName))
+                {
+                    if (stream == null)
+                    {
+                        return null;
+                    }
+
+                    var block = new byte[stream.Length];
+
+                    try
+                    {
+                        stream.Read(block, 0, block.Length);
+                        return Assembly.Load(block);
+                    }
+                    catch (IOException)
+                    {
+                        return null;
+                    }
+                    catch (BadImageFormatException)
+                    {
+                        return null;
+                    }
+                }
+            }
+            return null;
         }
     }
 }
